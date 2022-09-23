f634cba47228bfbdf28b483b374a072c85517a9a
Anthony Trummer <79271220+tony-ds@users.noreply.github.com>
2022-09-23 13:38:32 -0400

    Update listLengthCheck.js

--- checks/resource/listLengthCheck.js
+++ checks/resource/listLengthCheck.js
@@ -23,7 +23,7 @@ if (xmlPolicy) {
     		results.push({
 	        "line": `<${issue.name} domain="${issue.attributes.domain}" name="${issue.attributes.name}" value="${issue.attributes.value}" />`,
 	        "title": "List length limit may be too high",
-	        "description": `The policy is setting a list length limit over <code>32</code>. This policy is used to determine the maximum number of images in a sequence space that can be processed for the final image. ImageMagick will always attempt to allocate the maximum necessary resources, but your system may be temporarily sluggish or unavailable or ImageMagick may abort. Note that depending on your use or environment, this value could still be acceptable.`
+	        "description": `The policy is setting a list length limit over <code>32</code>. This policy is used to determine the maximum number of images in a sequence space that can be processed for the final image. ImageMagick will always attempt to allocate the maximum necessary resources, but your system may be temporarily sluggish or unavailable, or ImageMagick may abort. Note that depending on your usage or environment, this value could still be acceptable.`
 	    	});
     	
     	return results;
@@ -31,7 +31,7 @@ if (xmlPolicy) {
 	    return {
 	        "line": `<policy domain="resource" name="list-length" value="???"/>`,
 	        "title": "List length limit is missing",
-	        "description": `The policy is not setting any list length limit. The default limit is <code>32</code>, but it is adjusted relative to the available resources on the machine if this information is available. This policy is used to determine the maximum number of images in a sequence space that can be processed for the final image. Note that depending on your use or environment, not setting a value could still be acceptable, even if discouraged.`
+	        "description": `The policy is not setting any list length limit. The default limit is <code>32</code>, but it is adjusted relative to the available resources on the machine if this information is available. This policy is used to determine the maximum number of images in a sequence space that can be processed for the final image. Note that depending on your usage or environment, not setting a value could still be acceptable, even if discouraged.`
 	    }
 	}
 
