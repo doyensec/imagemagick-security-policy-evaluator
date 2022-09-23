808707651acfeee968d09a679a8fe2352fe52255
Anthony Trummer <79271220+tony-ds@users.noreply.github.com>
2022-09-23 13:54:02 -0400

    Update maxMemoryLimitingCheck.js

--- checks/system/maxMemoryLimitingCheck.js
+++ checks/system/maxMemoryLimitingCheck.js
@@ -40,7 +40,7 @@ if (xmlPolicy) {
     		results.push({
 	        "line": `<${issue.name} domain="${issue.attributes.domain}" name="${issue.attributes.name}" value="${issue.attributes.value}" />`,
 	        "title": "Maximum allocable request limit may be too high",
-	        "description": `The policy is setting a memory request limit in bytes over <code>256 MiB</code>. This policy is used to determine the maximum amount of memory space in byte that is permitted for allocation requests. Some image processing algorithms (e.g. wavelet transform) might consume a substantial amount of memory to complete. ImageMagick maintains a separate memory pool for these large resource requests and as of 7.0.6-1 permits you to set a maximum request limit. If the limit is exceeded, an exception is thrown and the processing stops. This limit can be set by the maximum memory request by policy. Note that depending on your use or environment, these values could still be acceptable.`
+	        "description": `The policy is setting a memory request limit in bytes over <code>256 MiB</code>. This policy is used to determine the maximum amount of memory space in bytes that is permitted for allocation requests. Some image processing algorithms (e.g. wavelet transform) might require a substantial amount of memory to complete. ImageMagick maintains a separate memory pool for these large resource requests and as of 7.0.6-1 permits you to set a maximum request limit. If the limit is exceeded, an exception is thrown and the processing stops. This limit can be set by the maximum memory request by policy. Note that depending on your usage or environment, these values could still be acceptable.`
 	    	});
     	
     	return results;
@@ -48,7 +48,7 @@ if (xmlPolicy) {
 	    return {
 	        "line": `<policy domain="system" name="max-memory-request" value="???" />`,
 	        "title": "Maximum allocable request limit is missing",
-            "description": `The policy is not setting any memory request limit in bytes. The default limit is <code>256 MiB</code>. This policy is used to determine the maximum amount of memory space in byte that is permitted for allocation requests. Some image processing algorithms (e.g. wavelet transform) might consume a substantial amount of memory to complete. ImageMagick maintains a separate memory pool for these large resource requests and as of 7.0.6-1 permits you to set a maximum request limit. If the limit is exceeded, an exception is thrown and the processing stops. This limit can be set by the maximum memory request by policy. Note that depending on your use or environment, not setting a value could still be acceptable, even if discouraged.`
+            "description": `The policy is not setting any memory request limit in bytes. The default limit is <code>256 MiB</code>. This policy is used to determine the maximum amount of memory space in byte that is permitted for allocation requests. Some image processing algorithms (e.g. wavelet transform) might require a substantial amount of memory to complete. ImageMagick maintains a separate memory pool for these large resource requests and as of 7.0.6-1 permits you to set a maximum request limit. If the limit is exceeded, an exception is thrown and the processing stops. This limit can be set by the maximum memory request by policy. Note that depending on your usage or environment, not setting a value could still be acceptable, even if discouraged.`
 	    }
 	}
 
